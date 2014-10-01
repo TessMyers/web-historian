1ef03d10af2e8bb851a646549b991079fdd0426a
TessMyers <tcymyers@gmail.com>
2014-10-01 16:47:01 -0700

    mostly works. Hopefully no humans ever want to use this service ever

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,63 @@
-var http = require("http");
+var http = require("http-request");
 var handler = require("./request-handler");
+var path = require("path");
+var express = require("express");
+var fs = require('fs');
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+var server = express();
+
+var newPath = path.join(__dirname, '../archives/sites/'); //TODO fix this
+server.use(express.static(newPath));
+
+server.get("*", function (req, res){
+  console.log("get on nonexistent site.");
+});
+
+server.post("*", function (req, res) {
+  // obtain filepath to new file: URL
+  var datURL = "";
+  req.on("data", function(buf){
+    datURL += buf;
+  });
+
+  req.on("end", function() {
+    var filepath = newPath + datURL;
+
+    // scrape URL for HTML
+    http.get(datURL , function (e, scraped){
+      if (e) {
+        console.log("error getting html");
+        return;
+      }
+
+      var scrapedHTML = new Buffer(scraped.buffer);
+
+      // open and write new file
+      fs.open(filepath, "w", function (err, fd){
+
+        if (err) {
+          throw "cannot open file" + err;
+        } else {
+          // blatantly stolen from stack overflow
+          fs.write(fd, scrapedHTML, 0, scrapedHTML.length, null, function(err) {
+            if (err) {
+              throw 'error writing file: ' + err;
+            } else {
+              fs.close(fd, function() {
+                console.log('file written');
+              });
+            }
+          });
+          // </stolen>
+        }
+        res.end();
+      });
+    });
+  });
+});
+
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,13 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var express = require('express');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+
+  if (req.type === "")
+
   res.end(archive.paths.list);
 };
+
+/// LALALALALA THIS FILE DOES NOTHING
