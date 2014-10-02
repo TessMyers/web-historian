ad1168a876c173d228b48edad43560975edded31
TessMyers <tcymyers@gmail.com>
2014-10-02 15:50:36 -0700

    converted to http node, serves up really shitty looking sites

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,8 +1,8 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var httpRequest = require("http-request");
 
-// this file is now useless.
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
@@ -12,11 +12,43 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
+exports.createFile = function(req, res, datURL, filepath) {
+
+  httpRequest.get(datURL, function(e, scraped) { // scrapePage(e, scraped, filepath)
+    if (e) {
+      console.log("error getting html");
+      return;
+    }
+
+    var scrapedHTML = new Buffer(scraped.buffer);
+
+    // open and write new file
+    fs.open(filepath, "w", function(err, fd) {
+
+      if (err) {
+        throw "cannot open file" + err;
+      } else {
+        // blatantly stolen from stack overflow
+        fs.write(fd, scrapedHTML, 0, scrapedHTML.length, null, function(err) {
+          if (err) {
+            throw 'error writing file: ' + err;
+          } else {
+            fs.close(fd, function() {
+              console.log('file written');
+            });
+          }
+        });
+      }
+      console.log('writing headers after post request');
+
+      res.writeHead(201, exports.headers);
+      res.end();
+    });
+  });
+};
+
+
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
 };
-
-
-
-// As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,13 +1,78 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-var express = require('express');
-// require more modules/folders here!
+var httpRequest = require("http-request");
+var helpers = require('http-helpers');
+var url = require('url');
+var fs = require('fs');
+
+
+var sitePath = path.join(__dirname, '../archives/sites/');
+
+
+var getRequest = function (req, res, parsedUrl, filepath) {
+
+  if (filepath === undefined) {
+    console.log("careless get request");
+    return postRequest(req, res, parsedUrl);
+  }
+
+  var content = fs.readFileSync(filepath); //dontdothat.jpg
+
+  console.log('writing headers after get request');
+  res.writeHead(202,helpers.headers);
+  res.end(content);
+  // put this shit on the interwobs
+};
+
+
+var postRequest = function (req, res, parsedUrl){
+  console.log("post");
+
+  var datURL = "";
+  req.on("data", function(buf){
+    datURL += buf;
+
+    if (datURL.indexOf("=") !== -1) {
+      datURL = datURL.split("=",2)[1];
+      console.log("daturl", datURL);
+    }
+  });
+
+  req.on("end", function() {
+    var filepath = sitePath + datURL;
+
+    // first thing we do is open the file for reading.
+    // if there's an error, the file doesn't exist.
+
+    fs.open(filepath, "r", function(err, fd){
+      if (err) {
+
+        helpers.createFile(req, res, datURL, filepath);
+        console.log('file does not exist, writing');
+
+      } else {
+
+        fs.close(fd, function (){ console.log("file exists~~"); });
+
+        getRequest(req, res, parsedUrl, filepath);
+
+      }
+    });
+
+  });
+};
+
+
 
 exports.handleRequest = function (req, res) {
 
-  if (req.type === "")
+  var parsedUrl = url.parse(req.url);
 
-  res.end(archive.paths.list);
+  ({"GET": getRequest, "POST": postRequest})[req.method](req, res, parsedUrl);
+
+  // res.end(archive.paths.list);
 };
+
+exports.handler = exports.handleRequest;
 
 /// LALALALALA THIS FILE DOES NOTHING
